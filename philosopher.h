69c973ac182d30bc463901d906563ac7ad5c6572
Ilan LINHARDT <ilinhard@e2r5p1.clusters.42paris.fr>
2022-09-20 08:43:51 +0200

    Add secure atoi

--- philosopher.h
+++ philosopher.h
@@ -6,17 +6,29 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 07:37:08 by ilinhard          #+#    #+#             */
-/*   Updated: 2022/09/20 07:44:15 by ilinhard         ###   ########.fr       */
+/*   Updated: 2022/09/20 08:16:04 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <semaphore.h>
+
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
+
+typedef struct s_conditions
+{
+	int	nb_philo;
+	int	time_death;
+	int	time_eat;
+	int	time_sleep;
+	int	nb_eat;
+}		t_conditions;
 
 #endif
--- philosphers.c
+++ philosphers.c
@@ -6,13 +6,53 @@
 /*   By: ilinhard <ilinhard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 00:43:38 by ilinhard          #+#    #+#             */
-/*   Updated: 2022/09/20 07:50:10 by ilinhard         ###   ########.fr       */
+/*   Updated: 2022/09/20 08:43:29 by ilinhard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-int	main(void)
+int	ft_atoi(char *str)
 {
+	int			i;
+	int			sig;
+	long long	nb;
+	int			count;
 
+	i = 0;
+	sig = 1;
+	nb = 0;
+	count = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	while (str[i + count] >= '0' && str[i + count] <= '9')
+	{
+		nb = (nb * 10) + (str[i + count] - '0');
+		count++;
+	}
+	if (count > 10 || (nb * sig) < 0 || nb > INT_MAX)
+		return (-1);
+	return ((int)nb * sig);
+}
+
+// void	ft_parsing(char **av, t_conditions *rules)
+// {
+// }
+
+int	main(int ac, char **av)
+{
+	t_conditions	*rules;
+
+	(void)rules;
+	(void)ac;
+	if (ac != 5 || ac != 6)
+		return (write(2, "Wrong numbers of args\n", 22), 1);
+	// ft_parsing(av, rules);
+	return (0);
 }
